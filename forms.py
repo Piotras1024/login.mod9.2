af5534b1ef76f2096d3cf29cccf72ac68e86befc
Piotr <piotr.w.dabrowski2@gmail.com>
2022-02-22 15:51:23 +0100

    nie działa email login

--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+
+class EmailPasswordForm(FlaskForm):
+    email_name = StringField('spokoj@gmail.com', validators=[DataRequired(), Email()])
+    password = StringField('okej', valdators=[DataRequired()])
