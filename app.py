400d0c5b8402a7cf785827312634c60b554bd665
Piotr <piotr.w.dabrowski2@gmail.com>
2022-02-22 16:00:13 +0100

    nie działą

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from forms import EmailPasswordForm
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "nininini"
 
-User = namedtuple("User", field_names=["email", "password"])
-user = User(email="john@black.com", password="black")
+User = namedtuple("User", field_names=["email_log", "password"])
+user = User(email_log="john@black.com", password="black")
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
@@ -14,10 +14,10 @@ def login():
     error = ""
     if request.method == "POST":
         if form.validate_on_submit():
-            if form.email_name.data == user.email and form.password.data == user.password:
+            if form.email_log.data == user.email_log and form.password.data == user.password:
                 return "You are logged id"
             else:
                 return "Wrong credentials!!"
         else:
             error = form.errors
-    return render_template("email_login.html", form=form, error=error)
+    return render_template("/email_login.html/", form=form, error=error)
